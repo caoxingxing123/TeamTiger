6fcea7b4088cbeb3dd861fa94efdb3f164b61656
caoxingxing19881226 <xxcao@xxcaotekiMacBook-Pro-2.local>
2016-07-26 15:42:44 +0800

    commit code

--- TeamTiger/AppDelegate.m
+++ TeamTiger/AppDelegate.m
@@ -17,10 +17,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
     if(!self.window){
         self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     }
+    [self initialMethods];
     ViewController *vc = [[ViewController alloc] init];
     self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
@@ -47,6 +47,11 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma -mark initial methods
+- (void)initialMethods {
+    
 }
 
 @end
